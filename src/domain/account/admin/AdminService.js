5f00d9485aca75991e92c96b0cb9a32ac007e20f
sondn3008 <sondn3008@gmail.com>
2022-05-25 21:07:36 +0700

    refactor api login admin

--- src/domain/account/admin/AdminService.js
+++ src/domain/account/admin/AdminService.js
@@ -2,11 +2,10 @@
 import autoBind from "auto-bind";
 
 import createAdmin from "./AdminFactory";
-import { hashPassword } from "../../../utils/Utility";
-import BaseService from "../../../../base/BaseService";
-import AdminRepository from "../../../infrastructure/account/admin/AdminRepository";
-import HttpError from "../../../utils/HttpError";
-import HttpResponse from "../../../utils/HttpResponse";
+// import { hashPassword } from "../../utils/Utility";
+import BaseService from "../../../base/BaseService";
+import AdminRepository from "../../infrastructure/admin/AdminRepository";
+import { validPassword, hashPassword, makeCode } from "../../../helper/Utility.js";
 
 const adminRepository = new AdminRepository();
 
@@ -25,14 +24,23 @@ class AdminService extends BaseService {
     // Validate data and create object
     const newAdmin = createAdmin(data);
     if (newAdmin.errMessage) {
-      return new HttpError({statusCode: 400, message: newAdmin.errMessage});
+      response.statusCode = 400;
+      response.json = {
+        message: newAdmin.errMessage,
+      };
+      return response;
     }
 
     // Check Email Exist
     const checkEmailResult = await this.repository.findOneByEmail(data.email);
 
-    if (!checkEmailResult.isSuccess) {
-      return new HttpError({statusCode: 400, message: "Email has already registered"});
+    if (checkEmailResult.isSuccess) {
+      response.statusCode = 400;
+      response.json = {
+        success: false,
+        message: "Email has already registered",
+      };
+      return response;
     }
 
     // HashPassword
@@ -41,12 +49,62 @@ class AdminService extends BaseService {
     // Create new admin
     const result = await this.repository.create(newAdmin.info);
     if (!result.isSuccess) {
-      return new HttpError(result.message);
+      response.statusCode = 500;
+      response.json = {
+        message: result.message,
+      };
+      return response;
     }
 
     response.json = result;
     response.statusCode = 200;
-    return new HttpResponse(result);
+    return response;
+  }
+
+  async loginAdmin(data) {
+    const response = {
+      json: null,
+      statusCode: null,
+    };
+
+    // thiếu validate cho login admin
+
+    // Check Email Exist
+    const admin = await this.repository.findOneByEmail(data.email);
+    if (!admin.isSuccess) {
+      response.statusCode = 400;
+      response.json = {
+        error: true,
+        message: "The email you entered is not registered.",
+      };
+      return response;
+    }
+
+    //Check Password
+    console.log("Password " + admin.data.password);
+    const isValid = await validPassword(data.password, admin.data.password);
+    if (!isValid) {
+      response.statusCode = 400;
+      response.json = {
+        error: true,
+        message: "The password you entered is not correct.",
+      };
+      return response;
+    }
+
+    //JWT
+    const token = jwt.sign({ id: admin.data.id }, process.env.JWT_SECRET, {
+      expiresIn: 10000000,
+    });
+
+    response.statusCode = 200;
+    response.json = {
+      success: true,
+      id: admin.id,
+      token: token,
+      expiresIn: 10000000,
+    }
+    return response;
   }
 }
 
